c8fa57a0312950b927b654a05187f9dfa7342115
Maksym Tsap <tsap.max.v@gmail.com>
2021-08-15 13:28:49 +0300

    Add copyPackageJsonToDist to postbuild

--- postbuild.js
+++ postbuild.js
@@ -9,13 +9,14 @@ if (typeof exports.default === 'function') {
 `.trim();
 
 const pathToDist = path.join(__dirname, 'dist');
+const pathToPackageJson = path.join(__dirname, 'package.json');
 const newline = '\n';
 
 async function appendToFile(filePath, text) {
   return await fs.promises.appendFile(filePath, `${text}${newline}`);
 }
 
-; (async () => {
+async function fixModuleResolution() {
   const files = await fs.promises.readdir(pathToDist);
   const jsFiles = files.filter(filename => filename.endsWith('.js'));
 
@@ -24,4 +25,13 @@ async function appendToFile(filePath, text) {
   for (const fullPath of fullPaths) {
     await appendToFile(fullPath, code);
   }
+}
+
+async function copyPackageJsonToDist() {
+  return await fs.promises.copyFile(pathToPackageJson, path.join(pathToDist, 'package.json'))
+}
+
+; (async () => {
+  await fixModuleResolution();
+  await copyPackageJsonToDist();
 })();

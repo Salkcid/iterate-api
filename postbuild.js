b090f37e9440e9de549521efa4be77c4f847b8f9
Maksym Tsap <tsap.max.v@gmail.com>
2021-08-15 16:36:31 +0300

    Simplify postbuild

--- postbuild.js
+++ postbuild.js
@@ -1,47 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const code = `
-if (typeof exports.default === 'function') {
-  module.exports = exports.default;
-  Object.assign(module.exports, exports);
-}
-`.trim();
-
 const pathToDist = path.join(__dirname, 'dist');
 const pathToPackageJson = path.join(__dirname, 'package.json');
 const pathToREADME = path.join(__dirname, 'README.md');
-const newline = '\n';
-
-async function appendToFile(filePath, text) {
-  return await fs.promises.appendFile(filePath, `${text}${newline}`);
-}
-
-async function fixModuleResolution() {
-  const files = await fs.promises.readdir(pathToDist);
-  const jsFiles = files.filter(filename => filename.endsWith('.js'));
-
-  const fullPaths = jsFiles.map(filename => path.join(pathToDist, filename));
-
-  for (const fullPath of fullPaths) {
-    await appendToFile(fullPath, code);
-  }
-}
-
-async function copyPackageJsonToDist() {
-  return await fs.promises.copyFile(pathToPackageJson, path.join(pathToDist, 'package.json'))
-}
-
-async function copyReadmeToDist() {
-  return await fs.promises.copyFile(pathToREADME, path.join(pathToDist, 'README.md'))
-}
 
 ; (async () => {
-  await fixModuleResolution();
-  console.log('=> Fixed module resolution');
-  await copyPackageJsonToDist();
+  await fs.promises.copyFile(pathToPackageJson, path.join(pathToDist, 'package.json'));
   console.log('=> Copy package.json to dist');
-  await copyReadmeToDist();
-  console.log('=> Copy README to dist');
-  console.log('=> postbuild DONE');
+  await fs.promises.copyFile(pathToREADME, path.join(pathToDist, 'README.md'));
+  console.log('=> Copy README.md to dist');
 })();
